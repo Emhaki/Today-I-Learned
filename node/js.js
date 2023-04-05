ce84ec7f2da389ca1bf94e08ab480ce1613d601d
Emhaki <mhmh779@naver.com>
2023-04-05 13:06:42 +0900

    callback

--- node/js.js
+++ node/js.js
@@ -1,13 +1,51 @@
-const log = {
-    info: function(info) {
-        console.log('info' + info);
-    },
-    warning: function(warning) {
-        console.log('warning' + warning);
-    },
-    error: function(error) {
-        console.log('error' + error);
-    }
+// 호이스팅(Hoisting)
+hello();
+
+function hello() {
+    console.log("Hello World");
 }
 
-module.exports = log;
+// 구조 분해 할당(Destructuring assignment)
+const user = {
+    name: 'emhaki',
+    age: 20
+}
+
+function getName({name}) {
+    const { name } = user;
+    return name;
+}
+
+function getEmail({email = '이메일이 없습니다.'}) {
+    return email;
+}
+
+console.log(getName(user)); // 'emhaki'
+console.log(getEmail(user)); // '이메일이 없습니다.'
+
+// 나머지 매개변수(Rest parameters)
+function sum(...rest) {
+    console.log(rest);
+    return rest.reduce(function(acc, cur) {
+        return acc + cur;
+    }, 0)
+}
+
+// 화살표 함수(Arrow functions)
+const sum = function() {}
+const sum = () => {}
+function sum(a, b) {
+    return a + b;
+}
+const sum = (a, b) => a + b;
+
+// 콜백(Callback)
+const a = callback => {
+    callback()
+    console.log('A')
+}
+
+const b = () => {
+    console.log('B')
+}
+a(b)
