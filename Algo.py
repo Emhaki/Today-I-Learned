5416b2b1ce54479e00713504ffde1d88cec9fed6
emhaki <mhmh779@naver.com>
2023-04-16 16:10:18 +0900

    algorithm

--- Algo.py
+++ Algo.py
@@ -2,33 +2,23 @@ import sys
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
-R, C, K = map(int, input().split())
-graph = [list(map(str, input().strip())) for _ in range(R)]
-answer = 0
-# [
-# ['.', '.', '.', '.'],
-# ['.', 'T', '.', '.'],
-# ['.', '.', '.', '.']
-# ]
+# N 시험장 갯수, A 응시자 수
+N = int(input())
+A = list(map(int, input().split()))
+# 총감독관 감시 응시자 수 B, 부감독관 감시 응시자 수 C
+B, C = map(int, input().split())
+cnt = 0
 
-dx = [0,0,-1,1]
-dy = [-1,1,0,0]
+for i in range(len(A)):
+    if A[i] >= B:
+        A[i] -= B
+        cnt += 1
+        
+        if A[i] % C == 0:
+            cnt += (A[i] // C)
+        else:
+            cnt += (A[i] // C) + 1
+    else:
+        cnt += 1
 
-def dfs(x, y, cnt):
-    global answer
-
-    if cnt == K and x == 0 and y == C - 1:
-        answer += 1
-    
-    for i in range(4):
-        nx = dx[i] + x
-        ny = dy[i] + y
-
-        if 0 <= nx < R and 0 <= ny < C and graph[nx][ny] == '.':
-            graph[nx][ny] = 'T'
-            dfs(nx, ny, cnt+1)
-            graph[nx][ny] = '.'
-
-graph[R-1][0] = 'T' # 왼쪽 아래 스타트
-dfs(R-1,0,1) # 왼쪽 아래 스타트
-print(answer)
+print(cnt)
