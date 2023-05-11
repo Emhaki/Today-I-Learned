6257b5f5ed69be8c301c9d4870608369bc2c7b20
emhaki <mhmh779@naver.com>
2023-05-11 22:17:00 +0900

    algo

--- Algo.py
+++ Algo.py
@@ -2,23 +2,31 @@ import sys
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
-# N 시험장 갯수, A 응시자 수
-N = int(input())
-A = list(map(int, input().split()))
-# 총감독관 감시 응시자 수 B, 부감독관 감시 응시자 수 C
-B, C = map(int, input().split())
-cnt = 0
+from collections import deque
 
-for i in range(len(A)):
-    if A[i] >= B:
-        A[i] -= B
-        cnt += 1
-        
-        if A[i] % C == 0:
-            cnt += (A[i] // C)
-        else:
-            cnt += (A[i] // C) + 1
-    else:
-        cnt += 1
+R, C = map(int, input().split())
+graph = [list(map(str, input().strip())) for _ in range(C)]
+visited = [[0] * R for _ in range(C)]
+queue = deque()
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
 
-print(cnt)
+def bfs(i,j, wolf):
+    visited[i][j] = 1
+    queue.append((i,j))
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = dx[i] + x
+            ny = dy[i] + y
+
+            if 0 <= nx < R and 0 <= ny < C and 
+
+
+
+for i in range(R):
+    for j in range(C):
+        if graph[i][j] == 'v':
+            bfs(i,j)
