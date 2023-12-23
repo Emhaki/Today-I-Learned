db5b38eed54025a32cbd5948aeef53dff739c74d
emhaki <mhmh779@naver.com>
2023-12-23 14:36:05 +0900

    환경변수 설정

--- FastAPI/app/common/config.py
+++ FastAPI/app/common/config.py
@@ -1,48 +1,16 @@
-from dataclasses import dataclass
-from os import path, environ
-from typing import List
+import os
+from dotenv import load_dotenv
 
-base_dir = path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
+load_dotenv()
 
+class Settings:
+	
+    DB_USERNAME : str = os.getenv("USERNAME")
+    DB_PASSWORD = os.getenv("PASSWORD")
+    DB_HOST : str = os.getenv("HOST","localhost")
+    DB_PORT : str = os.getenv("PORT",3306)
+    DB_DATABASE : str = os.getenv("DATABASE")
+	
+    DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
 
-@dataclass
-class Config:
-    """
-    기본 Configuration
-    """
-    BASE_DIR: str = base_dir
-    DB_POOL_RECYCLE: int = 900
-    DB_ECHO: bool = True
-    DEBUG: bool = False
-    TEST_MODE: bool = False
-    DB_URL: str = environ.get("DB_URL", "mysql+pymysql://travis@localhost/notification_api?charset=utf8mb4")
-
-
-@dataclass
-class LocalConfig(Config):
-    TRUSTED_HOSTS = ["*"]
-    ALLOW_SITE = ["*"]
-    DEBUG: bool = True
-
-
-@dataclass
-class ProdConfig(Config):
-    TRUSTED_HOSTS = ["*"]
-    ALLOW_SITE = ["*"]
-
-
-@dataclass
-class TestConfig(Config):
-    DB_URL: str = "mysql+pymysql://travis@localhost/notification_test?charset=utf8mb4"
-    TRUSTED_HOSTS = ["*"]
-    ALLOW_SITE = ["*"]
-    TEST_MODE: bool = True
-
-
-def conf():
-    """
-    환경 불러오기
-    :return:
-    """
-    config = dict(prod=ProdConfig, local=LocalConfig, test=TestConfig)
-    return config[environ.get("API_ENV", "local")]()
+settings = Settings()
--- FastAPI/app/main.py
+++ FastAPI/app/main.py
@@ -1,25 +1,9 @@
-from dataclasses import asdict
-from typing import Union
-from app.database.conn import db
-
 from fastapi import FastAPI
-import uvicorn
 
-from FastAPI.app.common.config import conf
+app = FastAPI()
 
-def create_app():
-
-    c = conf()
-    app = FastAPI()
-    conf_dict = asdict(c)
-    db.init_app(app, **conf_dict)
-
-
-    # app.include_router(index.router)
-    return app
-
-
-app = create_app()
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+@app.get("/")
+def index():
+    return {
+        "FastAPI": "start"
+    }
